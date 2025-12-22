326f9b2e824d79675b4f348704025d4d7320c9d4
marzelion <smrtinez@student.42barcelona.com>
2025-12-22 14:56:51 +0100

    draft test of the main loop with readline and siqquit handler

---
+++ src/main.c
@@ -0,0 +1,75 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+/*
+	handler to block Ctrl+\
+*/
+void handler_sigaction(int sig)
+{
+	(void)sig;
+}
+
+/*
+	setup handler for crtl+\
+	TODO: Doesnt work
+*/
+void init_sigaction()
+{
+	struct	sigaction sa;
+	size_t	s;
+	char	**p;
+
+	s = 0;
+	p = (char **)&sa;
+	while (s < sizeof(sa))
+	{
+		*p++ = 0;
+		s++;
+	}
+	sa.sa_handler = &handler_sigaction;
+	sigaction(SIGQUIT, &sa, (void *)0);
+}
+
+void init()
+{
+	init_sigaction();
+}
+void update()
+{
+}
+
+void draw()
+{
+	write(1, "> ", 2);
+}
+
+/*
+	
+*/
+void readconsole_events()
+{
+	size_t	bytes_read;
+	char	read_buffer[6];
+
+	bytes_read = read(0, read_buffer, sizeof(read_buffer) - 1);
+	if ((bytes_read >1) && (read_buffer[0] == 'e' && read_buffer[1] == 'x'))
+		exit(0);
+}
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	(void)argc;
+
+	void init();
+
+	while(1)
+	{
+		update();
+		draw();
+		readconsole_events();
+	}
+
+	return (0);
+}
